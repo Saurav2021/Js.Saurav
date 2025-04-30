a7c2641d2fd23249b8df78d8d43da9bb00240f3f
Saurav2021 <87760382+Saurav2021@users.noreply.github.com>
2025-04-30 03:29:50 +0000

    Memory Elements

---
+++ 01_Basics/06_memory.js
@@ -0,0 +1,20 @@
+// Stack (Primitive) , heap(Non-Primitive)
+
+let myYoutubeName="Sauravkryoutube"
+
+let anothername=myYoutubeName
+anothername="Coding"
+
+console.log(myYoutubeName);
+console.log(anothername);
+
+let userone={
+    email:"userone@gmail.com",
+    upi:"user@ybl"
+}
+let userTwo=userone
+
+userTwo.email ="Saurav@gmail.com"
+
+console.log(userone.email);
+console.log(userTwo.email);
