c0dca7f482fb2c6b95477ddb7f6052854214e088
Saurav2021 <87760382+Saurav2021@users.noreply.github.com>
2025-05-03 07:32:47 +0000

    02_b_scopes

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -9,10 +9,51 @@ if (true){
     let a =10
     const b =20
     // var c =30
-    console.log("Inner:" ,a);
+    // console.log("Inner:" ,a);
 }
 
 
-console.log("Outer:" ,a);
+// console.log("Outer:" ,a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username="Saurav"
+
+    function two(){
+        const website ="Youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+
+if(true){
+    const username ="Saurav"
+    if(username==="Saurav"){
+        const website="youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+console.log(addone(5));
+
+function addone(num){
+    return num+1
+}
+
+
+
+addTwo(5)
+
+const addTwo=function(num){
+    return num +2
+}
+
