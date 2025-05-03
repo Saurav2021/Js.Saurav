f925880799d4c088268e3700ec928c1d48122159
Saurav2021 <87760382+Saurav2021@users.noreply.github.com>
2025-05-03 05:34:44 +0000

    05_objects

---
+++ 02_Basics/05_objects.js
@@ -0,0 +1,27 @@
+const course ={
+    coursename:"Js in Hindi",
+    price:"999",
+    courseInstructor:"Saurav"
+}
+
+// course.courseInstructor
+
+// const {courseInstructor}=course
+// console.log(courseInstructor);
+
+const{courseInstructor:instructor}=course
+console.log(instructor);
+
+//  API
+
+// {
+//     "name":"Saurav",
+//     "coursename":"Js in Hindi"
+//     "price":"free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
