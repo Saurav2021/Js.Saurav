e4716d309d833ce8f58d04e965b2cb2c825869de
Saurav2021 <87760382+Saurav2021@users.noreply.github.com>
2025-05-03 07:13:26 +0000

    02_scopes

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,18 @@
+// let a =10
+// const b =20
+// var c =3088
+
+let a =400
+
+
+if (true){
+    let a =10
+    const b =20
+    // var c =30
+    console.log("Inner:" ,a);
+}
+
+
+console.log("Outer:" ,a);
+// console.log(b);
+// console.log(c);
